87eef85cafdb9922f6f1abb58aaffa89d406bc4b
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-08-28 11:52:54 +0300

    Updates Fibinacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        return
+    if length == 1:
+        print('[0]')
+        return
+    if length == 2:
+        print('[0, 1]')
+        return
+    list = [0, 1]
+    for i in range(2, length):
+        list.append(list[i-1] + list[i-2])
+    print(list)
